dd13383ebde80dfc5cf23754202d12d559966045
upendra regmi <uregmi83@gmail.com>
2017-06-14 12:04:28 +0545

    some comments were changed

--- login_form/database.php
+++ login_form/database.php
@@ -2,17 +2,17 @@
 
 $host = 'localhost'; //localhost
 $user1 = 'root';   //user
-$pass = '';// no password
+$pass = '';// no password in xampp mysql database.
 $db = 'userpassword'; // database
 
 //connect to database..
 mysql_connect($host, $user1, $pass);
 mysql_select_db($db);
 if (isset($_POST['username'])) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+	$user_name = $_POST['username'];  //name of input username
+	$pass_word = $_POST['password'];  //name of input password
 	// "user_password" is TABLE...
-	$sql ="SELECT * FROM user_password WHERE user='".$username."' AND password='".$password."' LIMIT 1";
+	$sql ="SELECT * FROM user_password WHERE user='".$user_name."' AND password='".$pass_word."' LIMIT 1";
 	$res = mysql_query($sql);
 if (mysql_num_rows($res) == 1) {
 	echo "You have been logged in sucessfully....";
